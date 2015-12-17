4281320fd6655251601a3b7422c416faa3a4e643
Dragan Djuric <dragandj@gmail.com>
2015-12-17 20:57:27 +0100

    Some rendering improvements and bugfixes.

--- src/clojure/uncomplicate/bayadera/visual.clj
+++ src/clojure/uncomplicate/bayadera/visual.clj
@@ -6,72 +6,89 @@
 
 (defrecord HSBColor [^float h ^float s ^float b])
 
-(defn grid-style [^PGraphics g ^HSBColor color]
-  (doto g
-    (.beginDraw)
-    (.clear)
-    (.colorMode PConstants/HSB 360 100 100)
-    (.stroke (.h color) (.s color) (.b color))
-    (.endDraw))
-  g)
+(defn style
+  ([^PGraphics g ^HSBColor color ^long weight]
+   (doto g
+     (.beginDraw)
+     (.clear)
+     (.colorMode PConstants/HSB 360 100 100)
+     (.strokeWeight weight)
+     (.stroke (.h color) (.s color) (.b color))
+     (.endDraw))
+   g)
+  ([g color]
+   (style g color 1)))
 
-(defn grid [^PGraphics g ^long cell-width ^long cell-height]
-  (let [width (.width g)
-        height (.height g)
-        x-count (int (/ width cell-width))
-        y-count (int (/ height cell-height))]
+(defn grid [^PGraphics g ^long padding ^long x-offset ^long y-offset]
+  (let [x-max (dec (.width g))
+        y-max (dec (.height g))]
     (do
       (.beginDraw g)
-      (loop [x 0]
-        (if (< x width)
-          (do (.line g x height x 0)
-              (recur (+ x cell-width)))))
-      (loop [y (dec height)]
-        (if (<= 0 y)
-          (do (.line g 0 y width y)
-              (recur (- y cell-height)))))
+      (dotimes [i (inc (int (/ (- x-max padding) x-offset)))]
+        (.line g (+ padding (* i x-offset)) y-max
+               (+ padding (* i x-offset)) 0))
+      (dotimes [i (inc (int (/ (- y-max padding) y-offset)))]
+        (.line g 0 (- y-max padding (* i y-offset))
+               x-max (- y-max padding (* i y-offset))))
       (.endDraw g)
       g)))
 
-(defn x-axis [^PGraphics g ^long mark-height ^long cell-width]
-  (let [width (.width g)
-        x-count (int (/ width cell-width))]
-    (do
-      (.beginDraw g)
-      (.line g 0 0 width 0)
-      (loop [x 0]
-        (if (< x width)
-          (do (.line g x 0 x mark-height)
-              (recur (+ x cell-width)))))
-      (.endDraw g)
-      g)))
+(defn frame [^PGraphics g ^long margin]
+  (do
+    (.beginDraw g)
+    (.rectMode g PConstants/CORNER)
+    (.noFill g)
+    (.rect g margin margin
+           (- (dec (.width g)) (* 2 margin)) (- (dec (.height g)) (* 2 margin)))
+    (.endDraw g)
+    g))
 
-(defn y-axis [^PGraphics g ^long mark-width ^long cell-height]
-  (let [height (.height g)
-        width (dec (.width g))
-        mark-start (- width mark-width)
-        y-count (int (/ height cell-height))]
+(defn ticks [^PGraphics g margin padding tick-length x-offset y-offset]
+  (let [x-max (dec (.width g))
+        y-max (dec (.height g))
+        margin (int margin)
+        gap (+ margin (int padding))
+        tick-length (int tick-length)
+        x-offset (int x-offset)
+        y-offset (int y-offset)]
     (do
       (.beginDraw g)
-      (.line g width 0 width height)
-      (loop [y (dec height)]
-        (if (<= 0 y)
-          (do (.line g mark-start y width y)
-              (recur (- y cell-height)))))
+      (dotimes [i (inc (int (/ (- x-max gap margin) x-offset)))]
+        (.line g (+ gap (* i x-offset)) (- y-max margin)
+               (+ gap (* i x-offset)) (- y-max (- margin tick-length))))
+      (dotimes [i (inc (int (/ (- y-max gap margin) y-offset)))]
+        (.line g margin (- y-max gap (* i y-offset))
+               (- margin tick-length) (- y-max gap (* i y-offset))))
       (.endDraw g)
       g)))
 
 (defn points [^PGraphics g xs ys x-lower x-upper y-lower y-upper]
-  (let [height (.height g)
-        width (.width g)
+  (let [x-max (.width g)
+        y-max (.height g)
         x-lower (float x-lower)
-        x-upper (float y-upper)
+        x-upper (float x-upper)
         y-lower (float y-lower)
         y-upper (float y-upper)]
     (.beginDraw g)
     (.clear g)
     (dotimes [i (dim xs)]
       (.point g
-              (PApplet/map (entry xs i) x-lower x-upper 0 width)
-              (PApplet/map (entry ys i) y-lower y-upper height 0)))
+              (PApplet/map (entry xs i) x-lower x-upper 0 x-max)
+              (PApplet/map (entry ys i) y-lower y-upper y-max 0)))
+    (.endDraw g)))
+
+(defn circles [^PGraphics g xs ys x-lower x-upper y-lower y-upper]
+  (let [x-max (.width g)
+        y-max (.height g)
+        x-lower (float x-lower)
+        x-upper (float x-upper)
+        y-lower (float y-lower)
+        y-upper (float y-upper)]
+    (.beginDraw g)
+    (.clear g)
+    (dotimes [i (dim xs)]
+      (.ellipse g
+              (PApplet/map (entry xs i) x-lower x-upper 0 x-max)
+              (PApplet/map (entry ys i) y-lower y-upper y-max 0)
+              2 2))
     (.endDraw g)))
--- test/clojure/uncomplicate/bayadera/visual_test.clj
+++ test/clojure/uncomplicate/bayadera/visual_test.clj
@@ -8,43 +8,46 @@
              [native :refer [sv]]]
             [quil.core :as q]))
 
-(def rand-vect (fmap! (fn ^double [^double x] (rand 10.0)) (sv 10000)))
-(def pdf-vect (fmap! (fn ^double [^double x] (log x)) (copy rand-vect)))
+(def rand-vect (fmap! (fn ^double [^double x] (rand 10.0)) (sv 1000)))
+(def pdf-vect (fmap! (fn ^double [^double x] (log (inc x))) (copy rand-vect)))
 
-(def grid-color (->HSBColor 110 30 10))
-(def axis-color (->HSBColor 200 60 80))
+(def grid-color (->HSBColor 60 30 10))
+(def frame-color (->HSBColor 200 50 60))
 (def points-color (->HSBColor 320 100 100))
 
 (def graphics (atom nil))
 
 (defn setup []
-  (let [g (q/create-graphics 1090 630 :p2d)
-        x (q/create-graphics 1090 20 :p2d)
-        y (q/create-graphics 20 630 :p2d)
-        p (q/create-graphics 1090 630 :p2d)]
-    (grid-style g grid-color)
-    (grid g 20 20)
-    (grid-style x axis-color)
-    (x-axis x 4 100)
-    (grid-style y axis-color)
-    (y-axis y 4 100)
-    (grid-style p points-color)
-    (points p rand-vect pdf-vect 0 10 (- 5) 5)
+  (let [g (q/create-graphics 1010 550 :p2d)
+        f (q/create-graphics 1050 590 :p2d)
+        p (q/create-graphics 1050 590 :opengl)]
+    (style g grid-color)
+    (grid g 5 20 20)
+    (style f frame-color)
+    (frame f 20)
+    (ticks f 20 5 3 100 100)
+    (style p points-color 2)
+    (points p rand-vect pdf-vect 0 10 0 3)
     (reset! graphics
             {:grid g
-             :x-axis x
-             :y-axis y
+             :frame f
              :points p})))
 
 (defn draw []
   (let [gr @graphics]
     (q/background 0)
-    (q/translate 400 400)
-    (grid-style (:points gr) points-color)
-    (points (:points gr) rand-vect pdf-vect 0 10 (- 3) 1)
-    (q/image (:grid gr) 0 0)
-    (q/image (:y-axis gr) -19 0)
-    (q/image (:x-axis gr) 0 629)
+    (q/translate 100 100)
+
+    ;;(grid-style (:grid gr) grid-color)
+    ;;(grid (:grid gr) 5 20 20)
+    ;;(grid-style (:frame gr) frame-color)
+    ;;(frame (:frame gr) 20)
+    ;;(ticks (:frame gr) 20 5 3 100 100)
+    ;;(grid-style (:points gr) points-color)
+    (points (:points gr) rand-vect pdf-vect 0 10 0 3)
+    ;;(ticks (:frame gr) 20 40 60)
+    (q/image (:grid gr) 20 20)
+    (q/image (:frame gr) 0 0)
     (q/image (:points gr) 0 0)
     )
 
