fff019e24ca68497db23765fe01bfe9849abc715
Dragan Djuric <dragandj@gmail.com>
2016-07-24 18:08:11 +0200

    DBDA Ch17 example - added regression with 300 people.

--- test/clojure/uncomplicate/bayadera/examples/dbda/ch17/robust_linear_regression_test.clj
+++ test/clojure/uncomplicate/bayadera/examples/dbda/ch17/robust_linear_regression_test.clj
@@ -28,16 +28,18 @@
 (def all-data (atom {}))
 (def state (atom nil))
 
-(let [in-file (slurp (io/resource "uncomplicate/bayadera/examples/dbda/ch17/ht-wt-data-30.csv"))]
-  (let [data (loop [c 0 data (drop 1 (csv/read-csv in-file)) hw (transient [])]
-               (if data
-                 (let [[_ h w] (first data)]
-                   (recur (inc c) (next data)
-                          (-> hw
-                              (conj! (double (read-string h)))
-                              (conj! (double (read-string w))))))
-                 (op [c] (persistent! hw))))]
-    (def params (sv data))))
+(defn read-data [in-file]
+  (loop [c 0 data (drop 1 (csv/read-csv in-file)) hw (transient [])]
+    (if data
+      (let [[_ h w] (first data)]
+        (recur (inc c) (next data)
+               (-> hw
+                   (conj! (double (read-string h)))
+                   (conj! (double (read-string w))))))
+      (op [c] (persistent! hw)))))
+
+(def params-30 (sv (read-data (slurp (io/resource "uncomplicate/bayadera/examples/dbda/ch17/ht-wt-data-30.csv")))))
+(def params-300 (sv (read-data (slurp (io/resource "uncomplicate/bayadera/examples/dbda/ch17/ht-wt-data-300.csv")))))
 
 (def rlr-prior
   (cl-distribution-model [(slurp (io/resource "uncomplicate/bayadera/opencl/distributions/gaussian.h"))
@@ -54,31 +56,43 @@
 (defn analysis []
   (with-default-bayadera
     (with-release [prior (distribution rlr-prior)
-                   prior-dist (prior (sv 5 -200 100 2 4 0.001 1000))
-                   prior-sampler (sampler prior-dist {:limits (sge 2 4 [1 10 -400 100 -10 10 0.001 1000])})
-                   post (posterior "rlr_30" (rlr-likelihood (dim params)) prior-dist)
-                   post-dist (post params)
-                   post-sampler (sampler post-dist {:limits (sge 2 4 [1 10 -400 100 -10 10 0.001 1000])})]
-      (println (time (mix! prior-sampler {:step 256})))
-      (println (time (mix! post-sampler {:step 256})))
-      (histogram! post-sampler 100))))
+                   prior-dist (prior (sv 10 -100 100 0 10 0.001 1000))
+                   prior-sampler (sampler prior-dist {:limits (sge 2 4 [1 20 -400 100 -20 20 0.001 1000])})
+                   post-30 (posterior "rlr_30" (rlr-likelihood (dim params-30)) prior-dist)
+                   post-30-dist (post-30 params-30)
+                   post-30-sampler (sampler post-30-dist {:limits (sge 2 4 [1 20 -400 100 -20 20 0.001 1000])})
+                   post-300 (posterior "rlr_300" (rlr-likelihood (dim params-300)) prior-dist)
+                   post-300-dist (post-300 params-300)
+                   post-300-sampler (sampler post-300-dist {:limits (sge 2 4 [1 20 -400 100 -20 20 0.001 1000])})]
+      (println (time (mix! post-30-sampler {:step 256 :refining 20})))
+      (println (time (mix! post-300-sampler {:step 256 :refining 20})))
+      [(histogram! post-30-sampler 100)
+       (histogram! post-300-sampler 100)])))
 
 (defn setup []
   (reset! state
           {:data @all-data
-           :nu (plot2d (qa/current-applet) {:width 500 :height 500})
-           :b0 (plot2d (qa/current-applet) {:width 500 :height 500})
-           :b1 (plot2d (qa/current-applet) {:width 500 :height 500})
-           :sigma (plot2d (qa/current-applet) {:width 500 :height 500})}))
+           :nu-30 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :b0-30 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :b1-30 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :sigma-30 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :nu-300 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :b0-300 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :b1-300 (plot2d (qa/current-applet) {:width 350 :height 350})
+           :sigma-300 (plot2d (qa/current-applet) {:width 350 :height 350})}))
 
 (defn draw []
   (when-not (= @all-data (:data @state))
     (swap! state assoc :data @all-data)
     (q/background 0)
-    (q/image (show (render-histogram (:nu @state) @all-data 0)) 0 0)
-    (q/image (show (render-histogram (:b0 @state) @all-data 1)) 0 520)
-    (q/image (show (render-histogram (:b1 @state) @all-data 2)) 0 1040)
-    (q/image (show (render-histogram (:sigma @state) @all-data 3)) 520 0)))
+    (q/image (show (render-histogram (:nu-30 @state) (@all-data 0) 0)) 0 0)
+    (q/image (show (render-histogram (:b0-30 @state) (@all-data 0) 1)) 0 370)
+    (q/image (show (render-histogram (:b1-30 @state) (@all-data 0) 2)) 0 740)
+    (q/image (show (render-histogram (:sigma-30 @state) (@all-data 0) 3)) 0 1110)
+    (q/image (show (render-histogram (:nu-300 @state) (@all-data 1) 0)) 370 0)
+    (q/image (show (render-histogram (:b0-300 @state) (@all-data 1) 1)) 370 370)
+    (q/image (show (render-histogram (:b1-300 @state) (@all-data 1) 2)) 370 740)
+    (q/image (show (render-histogram (:sigma-300 @state) (@all-data 1) 3)) 370 1110)))
 
 (defn display-sketch []
   (q/defsketch diagrams
