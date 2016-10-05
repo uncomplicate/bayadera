ac2f6a600ec6190463a1eedbc8aa9837be3aa08f
Dragan Djuric <dragandj@gmail.com>
2016-10-05 16:34:09 +0200

    Added license notice to source files.

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Dragan Djuric. All rights reserved.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) or later
+;;   which can be found in the file LICENSE at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 (defproject uncomplicate/bayadera "0.1.0-SNAPSHOT"
   :description "Bayesian Inference and Probabilistic Machine Learning Library for Clojure"
   :author "Dragan Djuric"
